7781dc63b03a8fe41b9b3e40f4b7eaefbcd3f9b7
J.F. Omhover <jf.omhover@gmail.com>
2013-08-08 00:52:10 +0200

    corrected anomalies in the management of addresses
    the address is read from EEPROM, and the assvAddress is set 0x04
    (I2C_ADD_ASSV_1). in the previous version, the local address of the node
    used for Wire.begin was 0x04.

--- OnboardCommLayer/OnboardCommLayer.cpp
+++ OnboardCommLayer/OnboardCommLayer.cpp
@@ -26,14 +26,14 @@
 static uint8_t transmitBuffer[sizeof(nanosat_message_t)];
 
 OnboardCommLayer::OnboardCommLayer() {
-  assvAddress_ = 0x04;
-  nodeAddress_ = EEPROM.read(0x00);
+  assvAddress_ = I2C_ADD_ASSV_1;		// jfomhover on 07/08/2013 : should be the address of the master arduino in the Sat
+  nodeAddress_ = EEPROM.read(0x00);		// reads the address of the node from EEPROM
 }
 
 uint8_t OnboardCommLayer::sendMessage(nanosat_message_t msg) {
   memcpy(transmitBuffer, &msg, sizeof(nanosat_message_t));
-  Wire.begin(assvAddress_);
-  Wire.beginTransmission(assvAddress_);
+  Wire.begin(nodeAddress_); 				// jfomhover on 07/08/2013 : join the I2C bus as a slave with its node address in the Sat
+  Wire.beginTransmission(assvAddress_);		// transmit to the master arduino in the Sat
   Wire.write(transmitBuffer, sizeof(nanosat_message_t));
   Wire.endTransmission();
 }
